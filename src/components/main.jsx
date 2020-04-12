120d466c2068ae9c8e469aafc17e287fcf7daa75
chapkovski <chapkovski@gmail.com>
2020-04-11 21:28:27 -0400

    toward blinking

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,7 @@
 // renders footer
 import React, { useState, useEffect, useRef, useContext } from "react";
-import Left from './left'
-import Right from './right'
+import Side from './side'
+ 
 import Q from './q'
 import {Container, Row, Col} from 'react-bootstrap'
 import ParamsContext from '../params'
@@ -13,9 +13,9 @@ const Main = () => {
     return (
         <Container fluid>
             <Row>
-                <Col><Left/>   </Col>
+                <Col><Side/>   </Col>
                 <Col><Q/>  </Col>
-                <Col><Right/></Col>
+                <Col><Side/></Col>
             </Row>
         </Container>
     )
--- src/components/right.jsx
+++
@@ -1,9 +0,0 @@
-// renders footer
-import React, { useState, useEffect, useRef } from "react";
-import Elem from './elem'
-const right = ()=>{
-    return (
-        <Elem/>
-    )
-}
-export default right
